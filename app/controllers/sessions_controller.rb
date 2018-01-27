72349dd09407457a40f00219e052382e115bae06
yuusuke.nishimura <west246x@gmail.com>
2018-01-27 05:52:57 +0000

    some fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   def destroy
      session[:user_id] = nil
     flash[:success] = 'ログアウトしました。'
-    redirect_to root_urls
+    redirect_to root_url
   end
   private
 
